10b309c42cfef7e115f2a192bfe75c86f2c47e2a
vo41 <161795582+vo41@users.noreply.github.com>
2024-08-22 18:40:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 // script.js
-const map = L.map('map').setView([51.505, -0.09], 2); // Set initial view to the center of the world
+const map = L.map('map').setView([51.505, -0.09], 2); // Set initial view
 
-// Add a tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// Add a dark tile layer
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>'
 }).addTo(map);
 
 // script.js (continued)
+const squareIcon = L.icon({
+  iconUrl: 'square-flag.png', // Path to your square flag image
+  iconSize: [20, 20], // Size of the icon in pixels
+  iconAnchor: [10, 10], // Point of the icon which will correspond to marker's location
+  popupAnchor: [0, -10] // Point from which the popup should open relative to the iconAnchor
+});
+
 async function addLocation(placeName) {
   const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${placeName}&format=json`);
   const data = await response.json();
   if (data.length > 0) {
     const { lat, lon, display_name } = data[0];
-    L.marker([lat, lon])
+    L.marker([lat, lon], { icon: squareIcon })
       .addTo(map)
       .bindPopup(display_name)
       .openPopup();
